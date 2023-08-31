734bea52e6e77c8ea2b76f5f798fb70acd3dfd8c
A01746459 <A01746459@itesm.mx>
2023-08-31 17:37:47 -0600

    amonos
    vamonos pa

--- servidor.py
+++ servidor.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
-#generar un servidor
-servidorWeb = Flask(__name__)
+from flask import Flask, render_template,request,jsonify
+import numpy as np
+from joblib import load
+import os
+
+#Cargar el modelo
+dt=load('dt1.joblib')
+
+#Generar el servidor(Back-End)
+servidorWeb=Flask(__name__)
 
 @servidorWeb.route("/holamundo",methods=['GET'])
-def holamundo():
-        return render_template('pagina1.html')
+def formulario():
+	return render_template('pagina1.html')
+
+#Envio de datos a traves de JSON
+@servidorWeb.route('/modelo',methods=['POST'])
+def modeloPrediccion():
+	#Procesar los datos de entrada
+	contenido =request.json
+	print(contenido)
+	return jsonify({'resultado':'Hola'})
+	
 
 if __name__ == '__main__':
-        servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
-
+	servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
