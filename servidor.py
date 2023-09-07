7e343597db254c33addd51a70a34c9f1f01c346f
A01746459 <A01746459@itesm.mx>
2023-09-06 19:37:21 -0600

    yes yes yes yes yes ye sye ys y ey sye sy ey sy eys ye yw

--- servidor.py
+++ servidor.py
@@ -28,6 +28,20 @@ def modeloPrediccion():
 	resultado = dt.predict(datosEntrada.reshape(1,-1))
 	return jsonify({'resultado':str(resultado[0])})
 
+#Envio de datos a traves de JSON
+@servidorWeb.route('/modeloForm',methods=['POST'])
+def modeloForm():
+	#Procesar los datos de entrada
+	contenido =request.form
+	print(contenido)
+	datosEntrada=np.array([0.88,0,2.6,0.098,25,67,0.9968,1,2,
+		contenido['pH'],
+		contenido['sulphates'],
+		contenido['alcohol']
+	])
+	#utilizar el modelo
+	resultado = dt.predict(datosEntrada.reshape(1,-1))
+	return jsonify({'resultado':str(resultado[0])})
 
 if __name__ == '__main__':
 	servidorWeb.run(debug=False,host='0.0.0.0',port='8080')
